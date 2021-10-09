1108fa24e5d357727c3706a12abb4800f987e46d
MohitRanshoor <91651362+MohitRanshoor@users.noreply.github.com>
2021-10-08 20:41:36 -0700

    Delete C_Program directory

--- C_Program/miles.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-float cel,fer;
-fer = (100*9/5)+32;
-printf("\nfer%f",fer);
-return 0;
-}
-
--- C_Program/q6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-printf("m\n""o\n""h\n""i\n""t\n");
-
-return 0;
-}
-
