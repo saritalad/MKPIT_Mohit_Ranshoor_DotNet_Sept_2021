7b1a4bb54573f34bd9661db0a50387739c8e3c38
mohitranshoor <mohit9595021538@gmail.com>
2021-10-13 09:58:18 -0700

    practice

---
+++ C_Programing/month_Switch case.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+
+  {
+	  int MN;
+	  printf("Enter Month no");
+	  scanf ("%d",&MN);
+	  switch(MN)
+	  {
+	  case 1 : printf("JANAURY");
+	  break;  
+	   case 2 : printf("FEBRUARY");
+	  break; 
+	  case 3: printf("MARCH");
+	  break;  
+	   case 4 : printf("APRIL");
+	  break;  
+	   case 5 : printf("MAY");
+	  break;  
+	   case 6 : printf("JUNE");
+	  break; 
+	  case 7: printf("JULY");
+	  break;  
+	   case 8 : printf("AUGUST");
+	  break;  
+	   case 9 : printf("SEPTEMBER");
+	  break;  
+	   case 10 : printf("OCTOBER");
+	  break; 
+	  case 12: printf("DECEMBER");
+	  break; 
+	  case 11: printf("NOVEMBER");
+	  break;   
+	  
+   
+  
+     }	  
+		  return 0;
+		   }
+
--- C_Programing/print_your_name_10lines.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-printf("R" "\nO" "\nS" "\nH" "\nA" "\nN" "\nYASH" "\nWANT" "\nTHA" "\nKRE");
-return 0;
-} 
-
