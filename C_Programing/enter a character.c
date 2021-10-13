5290c402cc99325eeebd014b102f58e8c8fdcb5e
mohitranshoor <mohit9595021538@gmail.com>
2021-10-12 21:46:58 -0700

    Assignment

---
+++ C_Programing/enter a character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int num;
+	printf("ENTER the number:");
+	scanf("%d",&num);
+	printf("\nyou have entered:%d",num);
+	getch();
+	
+	
+
+	
+	return 0;
+}
+
---
+++ C_Programing/question_03.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  
+  for(i=1;i<=100;i++)
+  {
+     if(i%5==0)
+   {
+    printf("The number divisible by five is\n%d\n",i); 
+   }
+  }
+return 0;
+}
+
