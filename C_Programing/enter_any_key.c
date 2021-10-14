ccd7a60557b5bf87c52a4234c6ed03d912c799ca
mohitranshoor <mohit9595021538@gmail.com>
2021-10-14 08:17:26 -0700

    Assignment

---
+++ C_Programing/enter_any_key.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("ENTER ANY KEY\n\a");
+	
+
+	
+	return 0;
+}
+
---
+++ C_Programing/kgtogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float gram,kg;
+printf("\n enter the weight in gram");
+scanf("%f",&gram);
+kg=gram/1000;
+printf("the weight in kg is=%2.3f ",kg);
+return 0;
+}
+
