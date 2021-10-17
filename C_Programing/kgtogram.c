88c44534157eab980e73f1e06c6ea32735006264
saritalad <60264225+saritalad@users.noreply.github.com>
2021-10-17 19:41:47 +0530

    Update kgtogram.c
    Read Question Carefully and it is clear that you are not attending online session continuously , This exmple I have explained in class and corrected some other student's code (I think Nilesh)

--- C_Programing/kgtogram.c
+++ C_Programing/kgtogram.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main()
-{
-float gram,kg;
+{/* wrong answer ,I have already explained this in class room I want ans 5Kg 400gm for input value of gm =5400 
+5 Oct 2021
+Q1	WAP to enter weight in grams and convert into Kg Gm(Ex:5620 gm = 5Kg 620gm)
+*/
+int gram,kg;
 printf("\n enter the weight in gram");
 scanf("%f",&gram);
 kg=gram/1000;
